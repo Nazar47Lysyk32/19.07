d7244c764695d7baeae1f7511f07e6325087e0ee
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2023-08-07 15:31:52 +0300

    Add files via upload

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const int MAX_STUDENTS = 50;
+const int MAX_SUBJECTS = 10;
+
+class Student {
+public:
+    Student(const string& name) : name(name) {}
+
+    string getName() const { return name; }
+    void setMark(int subjectIndex, int mark) { marks[subjectIndex] = mark; }
+    int getMark(int subjectIndex) const { return marks[subjectIndex]; }
+
+private:
+    string name;
+    int marks[MAX_SUBJECTS] = { 0 };
+};
+
+int main() {
+    Student students[MAX_STUDENTS];
+    string subjects[MAX_SUBJECTS];
+
+    string studentsFileName, subjectsFileName;
+    cout << "Enter the filename for student list: ";
+    cin >> studentsFileName;
+    cout << "Enter the filename for subjects list: ";
+    cin >> subjectsFileName;
+
+    ifstream studentsFile(studentsFileName);
+    ifstream subjectsFile(subjectsFileName);
+
+    int numStudents, numSubjects;
+    studentsFile >> numStudents;
+    subjectsFile >> numSubjects;
+
+    for (int i = 0; i < numStudents; ++i) {
+        string studentName;
+        studentsFile >> studentName;
+        students[i] = Student(studentName);
+    }
+
+    for (int i = 0; i < numSubjects; ++i) {
+        subjectsFile >> subjects[i];
+    }
+
+    for (int i = 0; i < numStudents; ++i) {
+        for (int j = 0; j < numSubjects; ++j) {
+            int mark;
+            cin >> mark;
+            students[i].setMark(j, mark);
+        }
+    }
+
+    cout << "Student\t";
+    for (int i = 0; i < numSubjects; ++i) {
+        cout << subjects[i] << "\t";
+    }
+    cout << "Average" << endl;
+
+    for (int i = 0; i < numStudents; ++i) {
+        cout << students[i].getName() << "\t";
+        int totalMarks = 0;
+        for (int j = 0; j < numSubjects; ++j) {
+            cout << students[i].getMark(j) << "\t";
+            totalMarks += students[i].getMark(j);
+        }
+        double average = static_cast<double>(totalMarks) / numSubjects;
+        cout << average << endl;
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+class Airplane {
+public:
+    virtual void performFlight() const = 0;
+};
+
+class PassengerAirplane : public Airplane {
+public:
+    void performFlight() const override {
+        cout << "Passenger airplane is taking off with passengers onboard." << endl;
+    }
+};
+
+class CargoAirplane : public Airplane {
+public:
+    void performFlight() const override {
+        cout << "Cargo airplane is transporting goods." << endl;
+    }
+};
+
+class MilitaryAirplane : public Airplane {
+public:
+    void performFlight() const override {
+        cout << "Military airplane is on a tactical mission." << endl;
+    }
+};
+
+class Airport {
+public:
+    Airport() : numAirplanes(0) {}
+
+    void addAirplane(Airplane* airplane) {
+        if (numAirplanes < MAX_AIRPLANES) {
+            airplanes[numAirplanes++] = airplane;
+        }
+    }
+
+    void operateFlights() const {
+        for (int i = 0; i < numAirplanes; ++i) {
+            airplanes[i]->performFlight();
+        }
+    }
+
+private:
+    static const int MAX_AIRPLANES = 10;
+    Airplane* airplanes[MAX_AIRPLANES];
+    int numAirplanes;
+};
+
+int main() {
+    PassengerAirplane passengerPlane;
+    CargoAirplane cargoPlane;
+    MilitaryAirplane militaryPlane;
+
+    Airport airport;
+    airport.addAirplane(&passengerPlane);
+    airport.addAirplane(&cargoPlane);
+    airport.addAirplane(&militaryPlane);
+
+    airport.operateFlights();
+
+    return 0;
+}
